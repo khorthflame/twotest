1aa84c1ed6d8465dbaca354e9877229f90818009
new name xuh <1223@qq.com>
2018-09-25 17:51:06 +0800

    笔记
    笔记

---
+++ day6/data2.php
@@ -0,0 +1,5 @@
+<?php
+    //将获取的get请求参数中，key为name的值返回
+    echo $_GET['name'];
+    echo $_GET['age'];
+?>
---
+++ day6/data3.php
@@ -0,0 +1,9 @@
+<?php
+//获取参数
+    $param = $_POST['sname'];
+    $param1 = $_POST['age'];
+
+    //返回数据
+    echo $param,$param1;
+    
+?>
